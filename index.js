d737e176e8f9ad232668151aeda12160ed1502c1
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2022-12-24 03:58:29 +0300

    changed some styles

---
+++ index.js
@@ -0,0 +1,16 @@
+class Cat {
+    constructor (nickname, ownersName, tel, email, breed, feed, petsSex, comment, [img]) {
+        this.nickname = nickname;
+        this.ownersName = ownersName;
+        this.tel = tel;
+        this.email = email;
+        this.breed = breed;
+        this.feed = feed;
+        this.petsSex = petsSex;
+        this.comment = comment;
+        this.img = img;
+    }
+}
+button.onclick = function() {
+    let cat[i] = new Cat();
+}
